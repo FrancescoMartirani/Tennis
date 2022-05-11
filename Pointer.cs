ad73127a78a9325a35a03fe2c718f61c3e02b76d
FrancescoMartirani <104638397+FrancescoMartirani@users.noreply.github.com>
2022-05-11 16:52:53 +0200

    Add files via upload

---
+++ Pointer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class Pointer
+    {
+
+        public string convertPoint(int pointNumber)
+        {
+
+            string pointOutput = "";
+
+            switch (pointNumber)
+            {
+
+                case 0: pointOutput = "Amore"; break;
+                case 1: pointOutput = "15"; break;
+                case 2: pointOutput = "30"; break;
+                case 3: pointOutput = "40"; break;
+
+            }
+
+            return pointOutput;
+
+        }
+
+        public string getPoint(int punteggio1, int punteggio2)
+        {
+            string result = "";
+
+            if(punteggio1 >= 3 && punteggio2 >= 3)
+            {
+                if (punteggio1 == punteggio2)
+                {
+
+                    result = "Deuce";
+
+                }
+
+                else if (punteggio1 - punteggio2 == 1 || punteggio1 - punteggio2 == -1)
+                {
+
+                    if (punteggio1 > punteggio2)
+                    {
+
+                        
+                        result = $"Vantaggio-{punteggio2}";
+
+                    }
+
+                    else
+                    {
+
+                        result = $"{punteggio1}-Vantaggio";
+
+                    }
+
+                }
+
+            }
+
+            else{
+
+                 result = $"{convertPoint(punteggio1)}-{convertPoint(punteggio2)}";
+
+            }
+
+            return result;
+
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Test
+    {
+        private Pointer pointer;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            pointer = new Pointer();
+
+        }
+
+        [TestCase(3,0, "40-Amore")]
+        [TestCase(0,1, "Amore-15")]
+        [TestCase(2,3, "30-40")]
+        [TestCase(0,0, "Amore-Amore")]
+        [TestCase(5,5, "Deuce")]
+        [TestCase(5,6, "5-Vantaggio")]
+        public void Points(int punteggio1, int punteggio2, string expected)
+        {
+
+            Assert.AreEqual(pointer.getPoint(punteggio1, punteggio2),expected);
+        }
+    }
+}
